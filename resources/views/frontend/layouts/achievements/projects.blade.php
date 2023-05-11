ae8458e7f0ce7784d14e55856bc7d023661759ce
Steve Adrien <montheadrien@gmail.com>
2023-05-11 17:34:45 +0100

    Changed the Frontend

---
+++ resources/views/frontend/layouts/achievements/projects.blade.php
@@ -0,0 +1,154 @@
+<!--Project One Start-->
+<section class="project-one">
+    <div class="container">
+        <div class="section-title text-center">
+            <div class="section-title__tagline-box">
+                <span class="section-title__tagline">Recent work lists</span>
+            </div>
+            <h2 class="section-title__title">Recently Completed Our
+                <br> Clients <span>Projects</span></h2>
+        </div>
+        <div class="project-one__bottom">
+            <div class="project-one__carousel owl-carousel owl-theme thm-owl__carousel" data-owl-options='{
+                        "loop": true,
+                        "autoplay": false,
+                        "margin": 30,
+                        "nav": true,
+                        "dots": false,
+                        "smartSpeed": 500,
+                        "autoplayTimeout": 10000,
+                        "navText": ["<span class=\"icon-left-arrow\"></span>","<span class=\"icon-right-arrow\"></span>"],
+                        "responsive": {
+                            "0": {
+                                "items": 1
+                            },
+                            "768": {
+                                "items": 2
+                            },
+                            "992": {
+                                "items": 3
+                            },
+                            "1200": {
+                                "items": 3
+                            }
+                        }
+                    }'>
+                <!--Project One Single Start-->
+                <div class="item">
+                    <div class="project-one__single">
+                        <div class="project-one__img-box">
+                            <div class="project-one__img">
+                                <img src="/frontend/images/project/project-1-1.jpg" alt="">
+                            </div>
+                        </div>
+                        <div class="project-one__content">
+                            <p>Finance</p>
+                            <h3><a href="portfolio-details.html">International business
+                                    <br> development</a></h3>
+                            <div class="project-one__arrow">
+                                <a href="portfolio-details.html"><span class="icon-right-arrow"></span></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!--Project One Single End-->
+                <!--Project One Single Start-->
+                <div class="item">
+                    <div class="project-one__single">
+                        <div class="project-one__img-box">
+                            <div class="project-one__img">
+                                <img src="/frontend/images/project/project-1-2.jpg" alt="">
+                            </div>
+                        </div>
+                        <div class="project-one__content">
+                            <p>Finance</p>
+                            <h3><a href="portfolio-details.html">International business
+                                    <br> development</a></h3>
+                            <div class="project-one__arrow">
+                                <a href="portfolio-details.html"><span class="icon-right-arrow"></span></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!--Project One Single End-->
+                <!--Project One Single Start-->
+                <div class="item">
+                    <div class="project-one__single">
+                        <div class="project-one__img-box">
+                            <div class="project-one__img">
+                                <img src="/frontend/images/project/project-1-3.jpg" alt="">
+                            </div>
+                        </div>
+                        <div class="project-one__content">
+                            <p>Finance</p>
+                            <h3><a href="portfolio-details.html">International business
+                                    <br> development</a></h3>
+                            <div class="project-one__arrow">
+                                <a href="portfolio-details.html"><span class="icon-right-arrow"></span></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!--Project One Single End-->
+                <!--Project One Single Start-->
+                <div class="item">
+                    <div class="project-one__single">
+                        <div class="project-one__img-box">
+                            <div class="project-one__img">
+                                <img src="/frontend/images/project/project-1-1.jpg" alt="">
+                            </div>
+                        </div>
+                        <div class="project-one__content">
+                            <p>Finance</p>
+                            <h3><a href="portfolio-details.html">International business
+                                    <br> development</a></h3>
+                            <div class="project-one__arrow">
+                                <a href="portfolio-details.html"><span class="icon-right-arrow"></span></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!--Project One Single End-->
+                <!--Project One Single Start-->
+                <div class="item">
+                    <div class="project-one__single">
+                        <div class="project-one__img-box">
+                            <div class="project-one__img">
+                                <img src="/frontend/images/project/project-1-2.jpg" alt="">
+                            </div>
+                        </div>
+                        <div class="project-one__content">
+                            <p>Finance</p>
+                            <h3><a href="portfolio-details.html">International business
+                                    <br> development</a></h3>
+                            <div class="project-one__arrow">
+                                <a href="portfolio-details.html"><span class="icon-right-arrow"></span></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!--Project One Single End-->
+                <!--Project One Single Start-->
+                <div class="item">
+                    <div class="project-one__single">
+                        <div class="project-one__img-box">
+                            <div class="project-one__img">
+                                <img src="/frontend/images/project/project-1-3.jpg" alt="">
+                            </div>
+                        </div>
+                        <div class="project-one__content">
+                            <p>Finance</p>
+                            <h3><a href="portfolio-details.html">International business
+                                    <br> development</a></h3>
+                            <div class="project-one__arrow">
+                                <a href="portfolio-details.html"><span class="icon-right-arrow"></span></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!--Project One Single End-->
+            </div>
+        </div>
+    </div>
+</section>
+<!--Project One End-->
