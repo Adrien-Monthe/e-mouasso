76251d732ea7499e386e031ab2e4dc05a2ffe6be
Steve Adrien <montheadrien@gmail.com>
2023-05-11 12:18:34 +0100

    Changed the Frontend

---
+++ resources/views/frontend/layouts/counter-section.blade.php
@@ -0,0 +1,74 @@
+<!--Counter One Start-->
+<section class="counter-one">
+    <div class="counter-one__inner">
+        <div class="counter-one__shadow"></div>
+        <div class="counter-one__bg"
+             style="background-image: url(/frontend/images/backgrounds/counter-one-bg.jpg);"></div>
+        <div class="container">
+            <div class="row">
+                <div class="col-xl-5 col-lg-5">
+                    <div class="counter-one__left">
+                        <div class="section-title text-left">
+                            <div class="section-title__tagline-box">
+                                <span class="section-title__tagline">fun facts</span>
+                            </div>
+                            <h2 class="section-title__title">Consultancy Funfacts
+                                <br> in Great <span>Numbers</span></h2>
+                        </div>
+                        <p class="counter-one__text">Leverage agile frameworks to provide a robust synopsis for
+                            high level overviews. Iterative approaches to corporate strategy data foster to
+                            collaborative thinking.</p>
+                    </div>
+                </div>
+                <div class="col-xl-7 col-lg-7">
+                    <div class="counter-one__right">
+                        <ul class="counter-one__count-box list-unstyled">
+                            <li>
+                                <div class="counter-one__icon">
+                                    <span class="icon-checking"></span>
+                                </div>
+                                <div class="counter-one__count count-box">
+                                    <h3 class="count-text" data-stop="886" data-speed="1500">00</h3>
+                                </div>
+                                <p class="counter-one__text">Projects Completed</p>
+                            </li>
+                            <li>
+                                <div class="counter-one__icon">
+                                    <span class="icon-recommend"></span>
+                                </div>
+                                <div class="counter-one__count count-box">
+                                    <h3 class="count-text" data-stop="600" data-speed="1500">00</h3>
+                                </div>
+                                <p class="counter-one__text">Satisfied Customers</p>
+                            </li>
+                            <li>
+                                <div class="counter-one__icon">
+                                    <span class="icon-consulting"></span>
+                                </div>
+                                <div class="counter-one__count count-box">
+                                    <h3 class="count-text" data-stop="960" data-speed="1500">00</h3>
+                                </div>
+                                <p class="counter-one__text">Expert Consultants</p>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="counter-one__bottom">
+        <div class="container">
+            <div class="counter-one__bottom-inner">
+                <p class="counter-one__bottom-text">@lang('general.need-best') <a
+                        href="{{ route('contact', app()->getLocale()) }}">@lang('general.send-request')</a></p>
+                <div class="counter-one__call-box">
+                    <p>@lang('general.call') <a href="tel:00237699374564">+237 233-424-213</a></p>
+                    <div class="counter-one__call-icon">
+                        <span class="icon-telephone-1"></span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!--Counter One End-->
