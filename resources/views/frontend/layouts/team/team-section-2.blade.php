5738ad7a54b015211caf95a492f8e43f02f34ac6
Steve Adrien <montheadrien@gmail.com>
2023-06-01 09:33:35 +0100

    Changed the Frontend

---
+++ resources/views/frontend/layouts/team/team-section-2.blade.php
@@ -0,0 +1,133 @@
+<!--Team One Start-->
+<section class="team-one about-page-team">
+    <div class="container">
+        <div class="team-one__top">
+            <div class="row">
+                <div class="col-xl-7 col-lg-6">
+                    <div class="team-one__left">
+                        <div class="section-title text-left">
+                            <div class="section-title__tagline-box">
+                                <span class="section-title__tagline">@lang('about.meet_our_team')</span>
+                            </div>
+                            <h2 class="section-title__title">@lang('about.meet_the_people_behind')</h2>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xl-5 col-lg-6">
+                    <div class="team-one__right">
+                        <p class="team-one__text" style="text-align: justify">@lang('about.fax')</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="team-one__bottom">
+            <div class="row">
+                <!--Team One Single Start-->
+                <div class="col-xl-4 col-lg-4 wow fadeInUp" data-wow-delay="100ms">
+                    <div class="team-one__single">
+                        <div class="team-one__img-box">
+                            <div class="team-one__img">
+                                <img src="/frontend/images/team/team3.jpg" alt="">
+                            </div>
+                            <div class="team-one__hover-content">
+                                <div class="team-one__hover-arrow-box">
+                                    <a href="team-details.html" class="team-one__hover-arrow"><span
+                                            class="fas fa-share-alt"></span></a>
+                                    <ul class="list-unstyled team-one__social">
+                                        <li><a href="#"><i class="fab fa-facebook"></i></a></li>
+                                        <li><a href="#"><i class="fab fa-instagram"></i></a></li>
+                                    </ul>
+                                </div>
+                                <h3 class="team-one__hover-title"><a href="team-details.html">Nama Yanick</a>
+                                </h3>
+                                <p class="team-one__hover-sub-title">Juriste</p>
+                                <p class="team-one__hover-text">@lang('home.baf')
+                                </p>
+                            </div>
+                        </div>
+                        <div class="team-one__content">
+                            <div class="team-one__arrow-box">
+                                <a href="team-details.html" class="team-one__arrow"><span
+                                        class="fas fa-share-alt"></span></a>
+                            </div>
+                            <h3 class="team-one__title"><a href="team-details.html">Nama Yanick</a></h3>
+                            <p class="team-one__sub-title">Juriste</p>
+                        </div>
+                    </div>
+                </div>
+                <!--Team One Single End-->
+                <!--Team One Single Start-->
+                <div class="col-xl-4 col-lg-4 wow fadeInUp" data-wow-delay="200ms">
+                    <div class="team-one__single">
+                        <div class="team-one__img-box">
+                            <div class="team-one__img">
+                                <img src="/frontend/images/team/team1.jpg" alt="">
+                            </div>
+                            <div class="team-one__hover-content">
+                                <div class="team-one__hover-arrow-box">
+                                    <a href="team-details.html" class="team-one__hover-arrow"><span
+                                            class="fas fa-share-alt"></span></a>
+                                    <ul class="list-unstyled team-one__social">
+                                        <li><a href="#"><i class="fab fa-facebook"></i></a></li>
+                                        <li><a href="#"><i class="fab fa-instagram"></i></a></li>
+                                    </ul>
+                                </div>
+                                <h3 class="team-one__hover-title"><a href="team-details.html">Edmond Mouasso</a>
+                                </h3>
+                                <p class="team-one__hover-sub-title">Fondateur & Consultant</p>
+                                <p class="team-one__hover-text">@lang('home.bah')
+                                </p>
+                            </div>
+                        </div>
+                        <div class="team-one__content">
+                            <div class="team-one__arrow-box">
+                                <a href="team-details.html" class="team-one__arrow"><span
+                                        class="fas fa-share-alt"></span></a>
+                            </div>
+                            <h3 class="team-one__title"><a href="team-details.html">Edmond Mouasso</a></h3>
+                            <p class="team-one__sub-title">Fondateur & Consultant</p>
+                        </div>
+                    </div>
+                </div>
+                <!--Team One Single End-->
+                <!--Team One Single Start-->
+                <div class="col-xl-4 col-lg-4 wow fadeInUp" data-wow-delay="300ms">
+                    <div class="team-one__single">
+                        <div class="team-one__img-box">
+                            <div class="team-one__img">
+                                <img src="/frontend/images/team/team2.jpeg" alt="">
+                            </div>
+                            <div class="team-one__hover-content">
+                                <div class="team-one__hover-arrow-box">
+                                    <a href="team-details.html" class="team-one__hover-arrow"><span
+                                            class="fas fa-share-alt"></span></a>
+                                    <ul class="list-unstyled team-one__social">
+                                        <li><a href="https://www.facebook.com/francis.eboute"><i class="fab fa-facebook"></i></a></li>
+                                        <li><a href="https://instagram.com/fhelespirituojuriste?igshid=NTc4MTIwNjQ2YQ=="><i class="fab fa-instagram"></i></a></li>
+                                    </ul>
+                                </div>
+                                <h3 class="team-one__hover-title"><a href="team-details.html">Francis Eboute</a>
+                                </h3>
+                                <p class="team-one__hover-sub-title">Consultant</p>
+                                <p class="team-one__hover-text">@lang('home.bag')
+                                </p>
+                            </div>
+                        </div>
+                        <div class="team-one__content">
+                            <div class="team-one__arrow-box">
+                                <a href="team-details.html" class="team-one__arrow"><span
+                                        class="fas fa-share-alt"></span></a>
+                            </div>
+                            <h3 class="team-one__title"><a href="team-details.html">Francis Eboute</a></h3>
+                            <p class="team-one__sub-title">Consultant</p>
+                        </div>
+                    </div>
+                </div>
+                <!--Team One Single End-->
+            </div>
+        </div>
+
+    </div>
+</section>
+<!--Team One End-->
