2d34dcbe5d72c20de2904ec5038b117c9111bba8
Steve Adrien <montheadrien@gmail.com>
2023-06-07 13:00:12 +0100

    Changed the Frontend

--- lang/fr/home.php
+++ lang/fr/home.php
@@ -6,7 +6,7 @@ return [
     'legal_and_social_audit' => 'Audit juridique<br> & social',
     "consultation-juridique"=>"Consultations <br> Juridiques",
     'welcome-to-cabinet' => 'Bienvenue dans notre cabinet',
-    'welcome-to-cabinet-title' => 'Meilleure <span>Experience</span> en Conseil Juridique',
+    'welcome-to-cabinet-title' => 'Meilleure <strong>EXPERIENCE</strong> en Conseil Juridique',
     'nous-sommes' => "Nous sommes un Cabinet d’Avocats qui a pour ambitions de fournir à sa clientèle des prestations juridiques de haute qualité dans un environnement juridique et judiciaire très complexe.",
     'droit-affaires' => 'Droit des Affaires',
     'droit-maritime' => 'Droit Maritime et Aérien',
