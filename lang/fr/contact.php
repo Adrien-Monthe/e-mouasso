176d8fff18add70c708fd5f497ab4141e4874066
Steve Adrien <montheadrien@gmail.com>
2023-06-07 13:11:55 +0100

    Changed the Frontend

--- lang/fr/contact.php
+++ lang/fr/contact.php
@@ -5,7 +5,7 @@ return [
     'page-title' => 'Contact',
     'contact-us' => 'Contactez Nous',
     'have-questions' => 'Des Questions?',
-    'contact-with-us' => 'Nous sommes disponible',
+    'contact-with-us' => 'Nous sommes disponibles',
     'anytime' => '24/7',
     'talk-to-expert' => 'Parlez avec un expert',
     'visit-anytime'=>'Visitez nos locaux',
