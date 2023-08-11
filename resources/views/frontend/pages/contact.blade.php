450ccd40d8141519f6389dcac1b661c488c29432
Steve Adrien <montheadrien@gmail.com>
2023-08-11 12:30:38 +0100

    Changed the Frontend

--- resources/views/frontend/pages/contact.blade.php
+++ resources/views/frontend/pages/contact.blade.php
@@ -133,12 +133,9 @@
     <!--Contact Page End-->
 
     <!--Google Map Start-->
-    <section class="google-map-two">
-        <iframe
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4562.753041141002!2d-118.80123790098536!3d34.152323469614075!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80e82469c2162619%3A0xba03efb7998eef6d!2sCostco+Wholesale!5e0!3m2!1sbn!2sbd!4v1562518641290!5m2!1sbn!2sbd"
-            class="google-map__two" allowfullscreen></iframe>
 
-    </section>
+    <div class="mapouter"><div class="gmap_canvas"><iframe style="width: 100%" height="510" id="gmap_canvas" src="https://maps.google.com/maps?q=niki akwa Douala&t=&z=10&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe><a href="https://2yu.co">2yu</a><br><style>.mapouter{position:relative;text-align:right;height:510px;width:100%;}</style><a href="https://embedgooglemap.2yu.co">html embed google map</a><style>.gmap_canvas {overflow:hidden;background:none!important;height:510px;width:100%;}</style></div></div>
+
     <!--Google Map End-->
 
 
